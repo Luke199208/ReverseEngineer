df9384533ca1b523d82b661094ed81a273baa6c6
刘奇 <704321039@qq.com>
2018-08-10 16:40:46 +0800

    reverse

---
+++ src/main/java/com/luke/mapper/TbUserMapper.java
@@ -0,0 +1,30 @@
+package com.luke.mapper;
+
+import com.luke.bean.TbUser;
+import com.luke.bean.TbUserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TbUserMapper {
+    int countByExample(TbUserExample example);
+
+    int deleteByExample(TbUserExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TbUser record);
+
+    int insertSelective(TbUser record);
+
+    List<TbUser> selectByExample(TbUserExample example);
+
+    TbUser selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") TbUser record, @Param("example") TbUserExample example);
+
+    int updateByExample(@Param("record") TbUser record, @Param("example") TbUserExample example);
+
+    int updateByPrimaryKeySelective(TbUser record);
+
+    int updateByPrimaryKey(TbUser record);
+}
